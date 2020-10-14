4a87cbe162ca94dbccff356e93f50ce65c81f2f1
Oscar Andre <josueht15@gmail.com>
2020-10-14 11:21:03 -0500

    main

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
 func main()  {
-	
+	r := mux.NewRouter()
+	r.HandleFunc("/", indexRouter)
+	r.HandleFunc("/api/login", middleware.Login)
+	// r.HandleFunc("/validate", middleware.ValidateToken)
+	s := r.PathPrefix("/api").Subrouter()
+	routes.Routes(s)
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:4200", "http://localhost:4800"},
+		AllowCredentials: true,
+		AllowedMethods:   []string{"POST", "GET", "OPTIONS", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},
+	})
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = helper.PORT //localhost
+	}
+
+	handler := c.Handler(r)
+
+	fmt.Println("Server online!")
+
+	log.Fatal(http.ListenAndServe(":"+port, handler))
+}
+
+func indexRouter(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome!")
 }
