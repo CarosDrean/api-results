39a9d8ba9fdf651f5a9ac5f79fbcbdd147780d1a
Oscar Salcedo <josueht15@gmail.com>
2021-02-11 08:49:19 -0500

    getallcovid in route

--- router/service.go
+++ router/service.go
@@ -10,6 +10,7 @@ import (
 func serviceRoutes(s *mux.Router) {
 	ctrl := service.ServiceController{DB: db.ServiceDB{}}
 	s.HandleFunc("/all/{idProtocol}", mid.CheckSecurity(ctrl.GetAllPatientsWithProtocol)).Methods("GET")
+	s.HandleFunc("/all-covid/{docNumber}", mid.CheckSecurity(ctrl.GetAllCovid)).Methods("GET")
 	s.HandleFunc("/all-organization/{id}", mid.CheckSecurity(ctrl.GetAllPatientsWithOrganization)).Methods("GET")
 	s.HandleFunc("/all-organization/", mid.CheckSecurity(ctrl.GetAllPatientsWithOrganizationFilter)).Methods("POST")
 	s.HandleFunc("/filter/", mid.CheckSecurity(ctrl.GetAllPatientsWithProtocolFilter)).Methods("POST")
