dc8ffc5ae2fb6f6d7642e0fca51e88d077ed9181
Alexander Matta Pisconti <alexanderpisconti@gmail.com>
2023-12-05 14:21:15 -0500

    cambioAdmis

--- controller/file.go
+++ controller/file.go
@@ -404,6 +404,18 @@ func (c FileController) DownloadExcelMatriz(w http.ResponseWriter, r *http.Reque
 	http.ServeFile(w, r, filePath)
 }
 
+func (c FileController) DownloadExcelMatrizGrande(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var exs models.ExcelPetitionMatrizFile
+	_ = json.NewDecoder(r.Body).Decode(&exs)
+
+	filePath, err := c.ExcelMatrizGrande(exs)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	http.ServeFile(w, r, filePath)
+}
 
 func (c FileController) ExcelMatriz(exs models.ExcelPetitionMatrizFile) (string, error){
 	f := excelize.NewFile()
@@ -613,6 +625,170 @@ func (c FileController) ExcelMatriz(exs models.ExcelPetitionMatrizFile) (string,
 		//return
 	}
 
+	if err := f.SaveAs("\\\\HOLO-SERVIDOR\\archivos sistema_2\\TEMPORAL\\" + exs.OrganizationID + ".xlsx"); err != nil {
+		println(err.Error())
+	}
+
+	var filePath string
+
+	filePath = "\\\\HOLO-SERVIDOR\\archivos sistema_2\\TEMPORAL\\" + exs.OrganizationID + ".xlsx";
+
+	if len(filePath) == 0 {
+		return "", errors.New("no aceptado")
+	}
+
+	return filePath, nil
+}
+
+func (c FileController) ExcelMatrizGrande(exs models.ExcelPetitionMatrizFile) (string, error) {
+	f := excelize.NewFile()
+
+	streamWriter, err := f.NewStreamWriter("Sheet1")
+
+	if err != nil {
+		fmt.Println(err)
+		//return
+	}
+
+	styleID, err := f.NewStyle(&excelize.Style{
+		Alignment: &excelize.Alignment{Horizontal: "center", Vertical: "center", WrapText: true},
+	})
+
+	if err != nil {
+		fmt.Println(err)
+		//return
+	}
+
+	z := 1
+	a := [62]float64{4.29, 7.57, 8.57, 8.86, 7.86, 9, 10.86, 4.43, 8.57, 14.29, 10.29, 17.14, 10.57,
+					 7, 10.71, 8.57, 9, 10.57, 11, 11, 35.14, 14.71, 14.29, 12.86, 11, 11, 11,
+					 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+					 9.29, 9.43, 9.43, 5.14}
+
+	for _, p := range a {
+
+		if err := streamWriter.SetColWidth(z, z, p); err != nil {
+			fmt.Println(err)
+			//return
+		}
+
+		z = z + 1
+	}
+
+	header := []interface{}{}
+
+	for _, cell := range []string{
+		"DATOS GENERALES", "", "", "", "", "", "", "", "", "", "", "", "",
+		"RESULTADOS VAMO", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+	} {
+		header = append(header, cell)
+	}
+
+	if err := streamWriter.SetRow("A1", header, excelize.RowOpts{StyleID: styleID}); err != nil {
+		fmt.Println(err)
+		//return
+	}
+
+	header2 := []interface{}{}
+
+	for _, cell2 := range []string{
+		"", "Nro DNI/CE/PAS", "Apellido materno", "Apellido paterno", "Primer nombre", "Segundo nombre", "Fecha de nacimiento (dd/mm/aaaa)",
+		"Edad\n(años)", "Género", "Departamento de procedencia", "Direccion domiciliaria actualizada",
+		"Empresa", "Ocupación / Cargo",
+
+		"Tipo de EMO (EMPO, EMOA, EMOR, OTROS)", "Fecha de evaluación de EMO (dd/mm/aaaa)",
+		"Resultado de Aptitud Médica", "Fecha de vencimiento (dd/mm/aaaa)", "Observaciones a levantar en 01 mes",
+		"Observaciones a levantar en 03 meses", "Observaciones a levantar en 06 meses", "Resultado de Aptitud Específica (si es que aplica)",
+		"Restricción 01", "Restricción 02", "Restricción 03", "Restricción 04", "Restricción 05", "Restricción 06",
+
+		"HEMOGRAMA", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+		"Grupo sanguineo y Factor RH", "Glucosa (mg/dl)", "Hb glicosilada HbA1c (%)", "RPR",
+
+		"EXAMEN DE ORINA",
+	} {
+		header2 = append(header2, cell2)
+	}
+
+	if err := streamWriter.SetRow("A2", header2, excelize.RowOpts{StyleID: styleID}); err != nil {
+		fmt.Println(err)
+		//return
+	}
+
+	header3 := []interface{}{}
+
+	for _, cell3 := range []string{
+		"LEUCOCITOS - Cel/uL", "HEMATIES - Cel/uL", "HEMOGLOBINA - g/dL", "HEMATOCRITO - %", "NEUTROFILOS SEGMENTADOS - %", "NEUTROFILOS SEGMENTADOS - Cel/mL",
+		"PLAQUETAS - Cel/uL", "VCM - fL", "HCM - pg", "CCMH - g/dL", "RDW - %", "VPM - fL", "BASTONES - %", "LINFOCITOS - %", "MONOCITOS - %", "EOSINOFILOS - %",
+		"BASOFILOS - %", "METAMIELOCITOS - %", "MIELOCITOS - %", "PROMIELOCITOS - %", "BLASTOS - %", "BASTONES - Cel/mL", "LINFOCITOS - Cel/mL", "MONOCITOS - Cel/mL",
+		"EOSINOFILOS - Cel/mL", "BASOFILOS - Cel/mL", "MIELOCITOS - Cel/mL", "METAMIELOCITOS - Cel/mL", "PROMIELOCITOS - Cel/mL", "BLASTOS - Cel/mL",
+		"COMPROBACION RECUENTO 100%",
+	} {
+		header3 = append(header3, cell3)
+	}
+
+	if err := streamWriter.SetRow("AB4", header3, excelize.RowOpts{StyleID: styleID}); err != nil {
+		fmt.Println(err)
+		//return
+	}
+
+	header4 := []interface{}{}
+
+	for _, cell4 := range []string{
+		"COMPLETO", "", "", "", "", "" , "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+		"Toxicológico",
+	} {
+		header4 = append(header4, cell4)
+	}
+
+	if err := streamWriter.SetRow("BK3", header4, excelize.RowOpts{StyleID: styleID}); err != nil {
+		fmt.Println(err)
+		//return
+	}
+
+	merges := map[string]string{
+		"A1": "M1", "A2": "A11", "B2": "B11", "C2": "C11", "D2": "D11", "E2": "E11", "F2": "F11", "G2": "G11", "H2": "H11",
+		"I2": "I11", "J2": "J11", "K2": "K11", "L2": "L11", "M2": "M11",
+
+		"N1" : "AA1", "N2": "N11", "O2": "O11", "P2": "P11", "Q2": "Q11",
+		"R2": "R11", "S2": "S11", "T2": "T11", "U2": "U11", "V2": "V11", "W2": "W11", "X2": "X11", "Y2": "Y11", "Z2": "Z11",
+		"AA2": "AA11",
+
+		"AB1" : "BF1", "AB2" : "BF3",
+		"AB4" : "AB11", "AC4" : "AC11", "AD4" : "AD11", "AE4" : "AE11", "AF4" : "AF11", "AG4" : "AG11", "AH4" : "AH11", "AI4" : "AI11", "AJ4" : "AJ11", "AK4" : "AK11",
+		"AL4" : "AL11", "AM4" : "AM11", "AN4" : "AN11", "AO4" : "AO11", "AP4" : "AP11", "AQ4" : "AQ11", "AR4" : "AR11", "AS4" : "AS11", "AT4" : "AT11", "AU4" : "AU11",
+		"AV4" : "AV11", "AW4" : "AW11", "AX4" : "AX11", "AY4" : "AY11", "AZ4" : "AZ11",
+		"BA4" : "BA11", "BB4" : "BB11", "BC4" : "BC11", "BD4" : "BD11", "BE4" : "BE11", "BF4" : "BF11",
+
+		"BG1" : "CY1",
+		"BG2" : "BG11", "BH2" : "BH11", "BI2" : "BI11", "BJ2" : "BJ11",
+
+		"BK2" : "CT2",
+		"BK3" : "CJ3", "CK3" : "CT3",
+	}
+
+	for x, v := range merges {
+		if err := streamWriter.MergeCell(x, v); err != nil {
+			fmt.Println(err)
+			//return
+		}
+	}
+
+	if err := streamWriter.Flush(); err != nil {
+		fmt.Println(err)
+		//return
+	}
+
+	/*
+	if err := f.SetRowHeight("Sheet1", 2, 45); err != nil {
+		fmt.Println(err)
+		//return
+	}
+
+	if err := f.SetRowHeight("Sheet1", 4, 178.5); err != nil {
+		fmt.Println(err)
+		//return
+	}
+	*/
 	if err := f.SaveAs("\\\\HOLO-SERVIDOR\\archivos sistema_2\\TEMPORAL\\" + exs.OrganizationID + ".xlsx"); err != nil {
 		println(err.Error())
 	}
--- router/file.go
+++ router/file.go
@@ -10,6 +10,7 @@ func fileRoutes(s *mux.Router) {
 	ctrl := file.FileController{}
 	s.HandleFunc("/", mid.CheckSecurity(ctrl.DownloadPDF)).Methods("POST")
 	s.HandleFunc("/excel", mid.CheckSecurity(ctrl.DownloadExcelMatriz)).Methods("POST")
+	s.HandleFunc("/excel-ga", mid.CheckSecurity(ctrl.DownloadExcelMatrizGrande)).Methods("POST")
 	s.HandleFunc("/all", mid.CheckSecurity(ctrl.DownloadZIPOrganization)).Methods("POST")
 	s.HandleFunc("/send-file", mid.CheckSecurity(ctrl.UploadAndSendZipOrganization)).Methods("POST")
 }
